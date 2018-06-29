8ebe2eb7c77d1c40250ad5fa5dae12d658e1e5ce
zhaoqian <qianzhaoapollo@163.com>
2018-06-30 07:37:43 +0800

    DP

---
+++ DP/main.cc
@@ -0,0 +1,20 @@
+/*
+ * main.cc
+ *
+ *  Created on: 2018Äê6ÔÂ28ÈÕ
+ *      Author: super
+ */
+
+#include <iostream>
+using namespace std;
+
+int plus_a(int a = 10, int b = 20)
+{
+	return a+b;
+}
+
+int main()
+{
+	cout << plus_a() << endl;
+	return 0;
+}
